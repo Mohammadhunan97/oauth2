d1723e8f04118d9cd8453b18f57fc696a8c4136c
mohammad <you@example.com>
2017-11-07 04:54:10 -0500

    updated schema and finished crud functionality with local routes

---
+++ key.js
@@ -0,0 +1,11 @@
+module.exports = {
+	google: {
+		client_ID: '549422657403-05kudgvoa78ra75c8j69m1sg20dv533d.apps.googleusercontent.com',
+		client_secret: 'i1K7sNe-sHKN-Ly5L_mDvHJB'
+	},
+	facebook: {
+		client_ID: '363548087429452',
+		client_secret: '36f9fbbb76d448fd20ea9409736c425b'
+	},
+	encryptionKey: 'foobar and cats'
+}
